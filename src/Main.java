eb1b7642bcf15d3ad4318ad549f3915b7bfd50f2
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-17 10:14:34 +0900

    Lambda test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+@FunctionalInterface
+interface MathOperation {
+    int operate(int a, int b);
+
+}
+
 public class Main {
     public static void main(String[] args) {
+        MathOperation add = (a, b) -> a + b;
+        MathOperation multiply = (a, b) -> a * b;
+        System.out.println(add.operate(5, 3));
+        System.out.println(multiply.operate(5, 3));
 
     }
 }
